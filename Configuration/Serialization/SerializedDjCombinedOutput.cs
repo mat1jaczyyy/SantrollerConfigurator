eb730a5028290f400c27ae5aac59c6936b0995e7
Sanjay Govind <sanjay.govind9@gmail.com>
2022-10-06 22:19:34 +1300

    Migrate from JSON to protobuf

---
+++ Configuration/Serialization/SerializedDjCombinedOutput.cs
@@ -0,0 +1,31 @@
+using Avalonia.Media;
+using GuitarConfiguratorSharp.NetCore.Configuration.Combined;
+using GuitarConfiguratorSharp.NetCore.Configuration.Microcontrollers;
+using GuitarConfiguratorSharp.NetCore.Configuration.Outputs;
+using GuitarConfiguratorSharp.NetCore.ViewModels;
+using ProtoBuf;
+
+namespace GuitarConfiguratorSharp.NetCore.Configuration.Serialization;
+
+[ProtoContract(SkipConstructor = true)]
+public class SerializedDjCombinedOutput : SerializedOutput
+{
+    [ProtoMember(1)] public override SerializedInput? Input => null;
+    [ProtoMember(2)] public override Color LedOn { get; }
+    [ProtoMember(3)] public override Color LedOff { get; }
+    [ProtoMember(4)] public int Sda { get; }
+    [ProtoMember(5)] public int Scl { get; }
+
+    public SerializedDjCombinedOutput(Color ledOn, Color ledOff, int sda, int scl)
+    {
+        LedOn = ledOn;
+        LedOff = ledOff;
+        Sda = sda;
+        Scl = scl;
+    }
+
+    public override Output Generate(ConfigViewModel model, Microcontroller microcontroller)
+    {
+        return new DjCombinedOutput(model, microcontroller, Sda, Scl);
+    }
+}
