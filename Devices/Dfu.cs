89b8acdf2e11c6cdbc173825bd0d83ade0c11956
Sanjay Govind <sanjay.govind9@gmail.com>
2023-04-27 19:46:53 +1200

    no real reason to exclude the dfu jump logic, it will work for any programs besides the first one

--- Devices/Dfu.cs
+++ Devices/Dfu.cs
@@ -52,9 +52,62 @@ public class Dfu : IConfigurableDevice
 
     public bool DeviceAdded(IConfigurableDevice device)
     {
+        if (device is Dfu dfu) dfu.Launch();
+
         return false;
     }
 
+    public void Launch()
+    {
+        Trace.WriteLine("Launching!");
+#if Windows
+        var mcu = _args.Device.IdProduct == 0x2FF7 ? "at90usb82" : "atmega16u2";
+
+        var appdataFolder = AssetUtils.GetAppDataFolder();
+        var dfuExecutable = Path.Combine(appdataFolder, "platformio", "dfu-programmer.exe");
+        var process = new Process();
+        process.StartInfo.FileName = dfuExecutable;
+        process.StartInfo.Arguments = $"{mcu} launch --no-reset";
+#else
+        _args.Device.Open(out var device);
+        if (device != null)
+        {
+            var requestType = UsbCtrlFlags.Direction_In | UsbCtrlFlags.RequestType_Class |
+                              UsbCtrlFlags.Recipient_Interface;
+
+            var sp = new UsbSetupPacket(
+                (byte) requestType,
+                3,
+                0,
+                0,
+                8);
+            var buffer = new byte[8];
+            device.ControlTransfer(ref sp, buffer, buffer.Length, out var length);
+            Console.WriteLine(length);
+            buffer = new byte[] {0x04, 0x03, 0x01, 0x00, 0x00};
+            requestType = UsbCtrlFlags.Direction_Out | UsbCtrlFlags.RequestType_Class |
+                          UsbCtrlFlags.Recipient_Interface;
+
+            sp = new UsbSetupPacket(
+                (byte) requestType,
+                1,
+                0,
+                0,
+                buffer.Length);
+            device.ControlTransfer(ref sp, buffer, buffer.Length, out length);
+            Console.WriteLine(length);
+            sp = new UsbSetupPacket(
+                (byte) requestType,
+                1,
+                0,
+                0,
+                0);
+            device.ControlTransfer(ref sp, buffer, 0, out length);
+            Console.WriteLine(length);
+        }
+#endif
+    }
+    
     public Microcontroller GetMicrocontroller(ConfigViewModel model)
     {
         var board = Board;
