b87d8d1379e3c43f6770565584e3017aad6fe7c1
Sanjay Govind <sanjay.govind9@gmail.com>
2023-07-20 21:44:54 +1200

    show raw value in configure all inputs dialog

--- ViewModels/BindAllWindowViewModel.cs
+++ ViewModels/BindAllWindowViewModel.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using GuitarConfigurator.NetCore.Configuration.Inputs;
 using GuitarConfigurator.NetCore.Configuration.Outputs;
 using GuitarConfigurator.NetCore.Devices;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace GuitarConfigurator.NetCore.ViewModels;
 
@@ -27,6 +29,9 @@ public class BindAllWindowViewModel : ReactiveObject
 
         ContinueCommand = ReactiveCommand.CreateFromObservable(() => Close(true));
         AbortCommand = ReactiveCommand.CreateFromObservable(() => Close(false));
+        this.WhenAnyValue(x => x.Input.RawValue, x => x.IsAnalog)
+            .Select(s => s.Item2 ? s.Item1 : (s.Item1 * ushort.MaxValue))
+            .ToPropertyEx(this, x => x.RawValue);
 
         if (Model.Device is not Santroller santroller)
         {
@@ -54,6 +59,10 @@ public class BindAllWindowViewModel : ReactiveObject
     public bool Response { get; set; }
     public bool IsAnalog { get; }
     public string LocalisedName { get; }
+    
+    // ReSharper disable UnassignedGetOnlyAutoProperty
+    [ObservableAsProperty] public int RawValue { get; }
+    // ReSharper enable UnassignedGetOnlyAutoProperty
 
     private IObservable<Unit> Close(bool response)
     {
