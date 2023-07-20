95749f9076603a43043663a377e6fab8008891c5
Sanjay Govind <sanjay.govind9@gmail.com>
2023-07-20 19:29:59 +1200

    add timeout to tuh_control_xfer

--- Configuration/Types/UsbHostInputType.cs
+++ Configuration/Types/UsbHostInputType.cs
@@ -8,55 +8,54 @@ public enum UsbHostInputType
     [Description("A Button")] A,
     [Description("B Button")] B,
     [Description("Y Button")] Y,
+    
     [Description("Left Bumper")] LeftShoulder,
     [Description("Right Bumper")] RightShoulder,
+    
     [Description("Kick Pedal 1")] Kick1,
     [Description("Kick Pedal 2")] Kick2,
+    
     [Description("Back Button")] Back,
     [Description("Start Button")] Start,
+    
     [Description("Left Stick Click")] LeftThumbClick,
     [Description("Right Stick Click")] RightThumbClick,
+    
     [Description("Home Button")] Guide,
     [Description("Capture Button")] Capture,
     
-    [Description("Left Turntable Blue Fret")]
-    LeftBlue = 15,
-
-    [Description("Left Turntable Red Fret")]
-    LeftRed,
-
-    [Description("Left Turntable Green Fret")]
-    LeftGreen,
-
-    [Description("Right Turntable Blue Fret")]
-    RightBlue,
-
-    [Description("Right Turntable Red Fret")]
-    RightRed,
-
-    [Description("Right Turntable Green Fret")]
-    RightGreen,
+    [Description("Left Turntable Blue Fret")] LeftBlue = 15,
+    [Description("Left Turntable Red Fret")] LeftRed,
+    [Description("Left Turntable Green Fret")] LeftGreen,
+    [Description("Right Turntable Blue Fret")] RightBlue,
+    [Description("Right Turntable Red Fret")] RightRed,
+    [Description("Right Turntable Green Fret")] RightGreen,
     
     [Description("Solo Green Fret")] SoloGreen = 22,
     [Description("Solo Red Fret")] SoloRed,
     [Description("Solo Yellow Fret")] SoloYellow,
     [Description("Solo Blue Fret")] SoloBlue,
     [Description("Solo Orange Fret")] SoloOrange,
+    
     [Description("Green Fret")] Green,
     [Description("Red Fret")] Red,
     [Description("Yellow Fret")] Yellow,
     [Description("Blue Fret")] Blue,
     [Description("Orange Fret")] Orange,
+    
     [Description("D-pad Up")] DpadUp = 35,
     [Description("D-pad Down")] DpadDown,
     [Description("D-pad Left")] DpadLeft,
     [Description("D-pad Right")] DpadRight,
+    
     [Description("Left Trigger Axis")] LeftTrigger,
     [Description("Right Trigger Axis")] RightTrigger,
+    
     [Description("Left Joystick X Axis")] LeftStickX,
     [Description("Left Joystick Y Axis")] LeftStickY,
     [Description("Right Joystick X Axis")] RightStickX,
     [Description("Right Joystick Y Axis")] RightStickY,
+    
     [Description("Dpad Up Pressure")] PressureDPadUp,
     [Description("Dpad Right Pressure")] PressureDPadRight,
     [Description("Dpad Left Pressure")] PressureDPadLeft,
