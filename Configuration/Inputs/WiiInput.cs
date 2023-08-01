d19e355da229869785debdb904fa0356c5d93994
Sanjay Govind <sanjay.govind9@gmail.com>
2023-08-01 21:12:43 +1200

    convert dj turntable to int

--- Configuration/Inputs/WiiInput.cs
+++ Configuration/Inputs/WiiInput.cs
@@ -127,10 +127,10 @@ public class WiiInput : TwiInput
         {WiiInputType.DjEffectDial, "((wiiData[3] & 0xE0) >> 5 | (wiiData[2] & 0x60) >> 2) << 11"},
         {WiiInputType.DjStickX, "((wiiData[0] & 0x3F) - 0x20) << 10"},
         {WiiInputType.DjStickY, "((wiiData[1] & 0x3F) - 0x20) << 10"},
-        {WiiInputType.DjTurntableLeft, "(((wiiButtonsLow & 1) ? 32 : 1) + (0x1F - (wiiData[3] & 0x1F))) << 10"},
+        {WiiInputType.DjTurntableLeft, "((((wiiButtonsLow & 1) ? 32 : 1) + (0x1F - (wiiData[3] & 0x1F))) - 32) << 10"},
         {
             WiiInputType.DjTurntableRight,
-            "(((wiiData[2] & 1) ? 32 : 1) + (0x1F - ((wiiData[2] & 0x80) >> 7 | (wiiData[1] & 0xC0) >> 5 | (wiiData[0] & 0xC0) >> 3))) << 10"
+            "((((wiiData[2] & 1) ? 32 : 1) + (0x1F - ((wiiData[2] & 0x80) >> 7 | (wiiData[1] & 0xC0) >> 5 | (wiiData[0] & 0xC0) >> 3))) - 32) << 10"
         },
         {WiiInputType.DrawsomePenPressure, "(wiiButtonsLow | (wiiButtonsHigh & 0x0f) << 8)"},
         {WiiInputType.DrawsomePenX, "wiiData[0] | wiiData[1] << 8"},
@@ -486,10 +486,10 @@ public class WiiInput : TwiInput
                     WiiInputType.DjStickX => ((wiiData[0] & 0x3F) - 0x20) << 10,
                     WiiInputType.DjStickY => ((wiiData[1] & 0x3F) - 0x20) << 10,
                     WiiInputType.DjTurntableLeft =>
-                        (((wiiButtonsLow & 1) != 0 ? 32 : 1) + (0x1F - (wiiData[3] & 0x1F))) << 10,
-                    WiiInputType.DjTurntableRight => (((wiiData[2] & 1) != 0 ? 32 : 1) +
+                        ((((wiiButtonsLow & 1) != 0 ? 32 : 1) + (0x1F - (wiiData[3] & 0x1F))) - 32) << 10,
+                    WiiInputType.DjTurntableRight => ((((wiiData[2] & 1) != 0 ? 32 : 1) +
                                                       (0x1F - (((wiiData[2] & 0x80) >> 7) | ((wiiData[1] & 0xC0) >> 5) |
-                                                               ((wiiData[0] & 0xC0) >> 3)))) << 10,
+                                                               ((wiiData[0] & 0xC0) >> 3)))) - 32) << 10,
 
                     _ => RawValue
                 };
